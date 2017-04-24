af1a42ed20f8bd9d62432864f9265451055775d2
Kai Mies <develop@regnavi.de>
2017-04-24 11:16:32 +0200

    Add parsing functionallity

--- index.js
+++ index.js
@@ -3,9 +3,19 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
+  const acceptedLanguages = req.headers['accept-language'];
+  const language = acceptedLanguages.split(',')[0] || '';
 
+  const userAgent = req.headers['user-agent'];
+  const operatingSystem = userAgent.match(/\((.*?)\)/)[1] || '';
+
+  res.json({
+    ipaddress: req.ip,
+    language: language,
+    software: operatingSystem,
+  })
 });
 
-const port = process.env.PORT;
+const port = process.env.PORT || 8080;
 
 app.listen(port);
